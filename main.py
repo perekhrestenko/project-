7355d10295cfc0384c1870373c0496275294f2c6
perekhrestenko <k.perekhrestenko@gmail.com>
2025-05-09 23:16:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+
+name = input("Введи своє ім'я: ")
+age = input("Введи свій вік: ")
+
+print(f"Привіт {name}, тобі {age}!")
+age = int(input("Введи свій вік: "))
+
+if age > 18:
+    print("Вхід дозволено!")
+else:
+    print("Вхід заборонено!")
